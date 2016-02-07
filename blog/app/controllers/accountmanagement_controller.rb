76bfdd0a62a2402315e3b7d02170c5f8802f0509
Ubuntu <ubuntu@ip-172-31-25-173.us-west-2.compute.internal>
2016-02-07 22:12:31 +0000

    Update to current, includes changes kyle made to arrow options, and some things files related to devise. Still work to do on Account Management. the show.html.erb needs to be updated as new.html.erb was for the arrow changes.

--- blog/app/controllers/accountmanagement_controller.rb
+++ blog/app/controllers/accountmanagement_controller.rb
@@ -8,4 +8,17 @@ class AccountmanagementController < ApplicationController
 	def update
 	end
 	
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:notice] = "Account created!"
+		else
+			flash[:notice] = "There was an error creating the account."
+		end
+	end
+
+	def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation)
+	end
+
 end

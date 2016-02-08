bdae348540448bb48facf02ee7315f9365a4be43
Ubuntu <ubuntu@ip-172-31-25-173.us-west-2.compute.internal>
2016-02-08 01:49:09 +0000

    Admittedly terrible UX for account creation, password changes, and the account management page, but I just wanted to get it working for this release

--- blog/app/controllers/accountmanagement_controller.rb
+++ blog/app/controllers/accountmanagement_controller.rb
@@ -6,6 +6,20 @@ class AccountmanagementController < ApplicationController
 	end
 
 	def update
+		# Only update the password when the password they entered as current
+		# is correct
+		if User.authenticate(current_user.email, params[:password])
+			if params[:newpassword] == params[:newpassword2]
+				current_user.password = params[:newpassword]
+				current_user.save()
+				flash[:notice] = "You have successfully changed your password!"
+			else
+				flash[:notice] = "Passwords did not match!"
+			end
+		else
+			flash[:notice] = "Incorrect current password!"
+		end
+		redirect_to :back
 	end
 	
 	def create
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'admin/index'
 
-  default_url_options :host => "http://www.vps.cs.wmich.edu:61580"
+  default_url_options :host => "https://familymap.noip.me"
   
 
   get 'password_resets/new'
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  	root 'welcome#index'
+  root 'welcome#index'
    
 	get "/log-in" => "sessions#new"
 	post "/log-in" => "sessions#create"

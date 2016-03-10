f139914b06c30509277c310ed9fe30349356721a
beefcheese <christysteven@gmail.com>
2016-03-10 13:04:32 -0500

    Fix to migration error from when the migration script was changed manually...

--- blog/db/migrate/20160128221248_add_devise_to_users.rb
+++ blog/db/migrate/20160128221248_add_devise_to_users.rb
@@ -4,8 +4,8 @@ class AddDeviseToUsers < ActiveRecord::Migration
       ## Database authenticatable
 #      t.string :email,              null: false, default: ""
       t.change :email, :string,     :null => false, :default => "" 
-#      t.string :encrypted_password, null: false, default: ""
-      t.change :encrypted_password, :string,     :null => false, :default => "" 
+      t.string :encrypted_password, null: false, default: ""
+#      t.change :encrypted_password, :string,     :null => false, :default => "" 
 
       ## Recoverable
       t.string   :reset_password_token

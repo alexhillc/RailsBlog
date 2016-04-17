209b02f56b7ad4abe96b747513e63aae6bf55db4
Ubuntu <ubuntu@ip-172-31-39-169.us-west-2.compute.internal>
2016-04-17 13:04:27 +0000

    Added in config code for test/production environments for the password reset mailer

--- blog/config/environments/production.rb
+++ blog/config/environments/production.rb
@@ -1,6 +1,23 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Set up mailer based on instructions from
+  # https://rubyonrailshelp.wordpress.com/2014/01/02/setting-up-mailer-using-devise-for-forgot-password/
+  config.action_mailer.default_url_options = { :host => "familymap.noip.me" }
+  config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: :plain,
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
--- blog/config/environments/test.rb
+++ blog/config/environments/test.rb
@@ -1,6 +1,22 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Set up mailer based on instructions from
+  # https://rubyonrailshelp.wordpress.com/2014/01/02/setting-up-mailer-using-devise-for-forgot-password/
+  config.action_mailer.default_url_options = { :host => "familymaptest.noip.me" }
+  config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: :plain,
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
